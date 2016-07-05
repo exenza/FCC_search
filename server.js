23c3761083a433b033e0671cd390f4aa18db2614
Max Berta <massimo.berta@gmail.com>
2016-07-05 22:35:21 +0100

    Testing env var

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express")
+var app = express()
+//Serve a static file with instructions
+
+app.get("/search/*", function(req, res){
+  	var q=req.url.split('/search/')[1]
+    //res.send({"search":q})
+    res.send(process.end.ck)
+})
+
+app.use('/*', express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT || 8080)
