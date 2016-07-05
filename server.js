87ebb7f245cdd7cc3909e567f6e8adee23e96dfc
Max Berta <massimo.berta@gmail.com>
2016-07-05 22:45:27 +0100

    debugging heroku deployement

--- server.js
+++ server.js
@@ -10,4 +10,7 @@ app.get("/search/*", function(req, res){
 
 app.use('/*', express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 8080)
+server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function() {
+  var addr = server.address();
+  console.log("AUTAX available at ", addr.address + ":" + addr.port);
+});
