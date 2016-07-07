7ecc97465476aa77a14c3f7a45f552c4a52db328
Max Berta <massimo.berta@gmail.com>
2016-07-07 22:33:45 +0100

    env var

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require("express")
 var app = express()
-//Serve a static file with instructions
+//Check for enviromental variables
+if(!process.env.cx || !process.env.google_key){
+	console.log('Missing enviromental variables')
+	require('./api_auth.js')
+}
 
+//Initiate search
 app.get("/search/*", function(req, res){
   	var q=req.url.split('/search/')[1]
     //res.send({"search":q})
     res.send(process.env.cx)
 })
 
+//Serve a static file with instructions
 app.use('/*', express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function() {
